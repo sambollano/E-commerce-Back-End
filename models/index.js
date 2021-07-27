4e19ddaaa4a20ca2b0932cbfdeca714088126968
Samantha Arellano <Sambollano54@gmail.com>
2021-07-27 04:11:49 -0500

    .

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 // Categories have many Products
-
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'Product_id'
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const path = require('path');
-// import sequelize connection
-
+const sequelize = require('./config/connection')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,7 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
